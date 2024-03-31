c22b937054f9da115ea1e3b712762c4edb6babf0
Thomas Hirschberger <thomas.ch.hirschberger@gmx.de>
2024-03-31 18:54:54 +0200

    added RaspberryPi_3B device (all GPIOs with physical pins)

--- src/devices/index.ts
+++ src/devices/index.ts
@@ -1,3 +1,4 @@
 export { default as NanoPi_NEO3 } from './NanoPi_NEO3';
 export { default as RaspberryPi_2B } from './RaspberryPi_2B';
+export { default as RaspberryPi_3B } from './RaspberryPi_3B';
 export { default as Default } from './Default';
