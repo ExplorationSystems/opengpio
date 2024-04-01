d19d139d2b93b30a60b271f00a9fa22770dafc71
Tom Hirschberger <github@hirschberger.eu>
2024-04-01 07:09:09 +0200

    added RaspberryPi_400 device

--- src/devices/index.ts
+++ src/devices/index.ts
@@ -2,4 +2,5 @@ export { default as NanoPi_NEO3 } from './NanoPi_NEO3';
 export { default as RaspberryPi_2B } from './RaspberryPi_2B';
 export { default as RaspberryPi_3B } from './RaspberryPi_3B';
 export { default as RaspberryPi_4B } from './RaspberryPi_4B';
+export { default as RaspberryPi_400 } from './RaspberryPi_400';
 export { default as Default } from './Default';
